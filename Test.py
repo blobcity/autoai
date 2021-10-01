0b0943ed2f4a69e301b6f44da032b7dc2e5d83a8
TRD <tilakgo@gmail.com>
2021-10-01 16:25:00 +0530

    minor fix

--- Test.py
+++ Test.py
@@ -17,12 +17,14 @@
 This Python file is test file consisting of core code functionality.
 """
 import blobcity as bc
-
+import pandas as pd
 file_path,target="https://raw.githubusercontent.com/Thilakraj1998/Datasets_general/main/BreastCancer1.csv",'diagnosis'
 features=['radius_mean','texture_mean','smoothness_mean','compactness_mean','concavity_mean']
 
-model=bc.train(file_path,target,features=features)
+df=pd.read_csv(file_path)
+model=bc.train(df=df,target=target,features=features)
 
 model.stats()
 #print(model.predict([[1,1,1,1,1]]))
-print(model.features())
+#print(model.features())
+#model.save()
