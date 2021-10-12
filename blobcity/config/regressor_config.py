c5091ea62051567fbb89b29f4c66fbe66feba37d
Aniket <aniketrathod@2707gmail.com>
2021-10-12 11:19:05 +0530

    added radiusneigbhoursregressor

--- blobcity/config/regressor_config.py
+++ blobcity/config/regressor_config.py
@@ -15,126 +15,136 @@
 
 import numpy as np
 from sklearn.experimental import enable_hist_gradient_boosting
-from sklearn import tree,ensemble,svm,linear_model,neighbors
+from sklearn import tree, ensemble, svm, linear_model, neighbors
 """
 This python file consist of Class variable models to store detail regarding different model to be utilized for Regression problem
 """
+
+
 class regressor_config:
     """
     Class variable model consist of a model type key with a list datatype value.
     where the list consist of model class object and dictionary of parameters specific to the model
     """
-    models={
-        "svr":[
+    models = {
+        "svr": [
             svm.SVR,
             {
-                "C":{"int":[1,3]},
-                "gamma":{"str":['auto','scale']},
-                "degree":{"int":[1,3]},
-                "kernel":{"str":['rbf','poly','linear']}
-            }  
+                "C": {"int": [1, 3]},
+                "gamma":{"str": ['auto', 'scale']},
+                "degree":{"int": [1, 3]},
+                "kernel":{"str": ['rbf', 'poly', 'linear']}
+            }
         ],
-        "nusvr":[
+        "nusvr": [
             svm.NuSVR,
             {
-                "nu":{'float':[0.1,1.0]},
-                "gamma":{"str":['auto','scale']},
-                "degree":{"int":[1,3]},
-                "kernel":{"str":['rbf','poly','linear']}
+                "nu": {'float': [0.1, 1.0]},
+                "gamma":{"str": ['auto', 'scale']},
+                "degree":{"int": [1, 3]},
+                "kernel":{"str": ['rbf', 'poly', 'linear']}
             }
         ],
-        "linearsvr":[
+        "linearsvr": [
             svm.LinearSVR,
             {
-                "C":{"int":[1,3]},
-                "epsilon":{'float':[1e-3,1]},
-                "loss":{'str':['epsilon_insensitive', 'squared_epsilon_insensitive']},
-                'tol':{'float':[1e-3,0.1]},
+                "C": {"int": [1, 3]},
+                "epsilon":{'float': [1e-3, 1]},
+                "loss":{'str': ['epsilon_insensitive', 'squared_epsilon_insensitive']},
+                'tol':{'float': [1e-3, 0.1]},
             }
         ],
-        "decisiontree":[
+        "decisiontree": [
             tree.DecisionTreeRegressor,
             {
-                "criterion":{'str':['mse', 'friedman_mse', 'mae', 'poisson']},
-                "splitter":{'str':['random','best']},
-                "max_features":{"str":["auto", "sqrt", "log2"]},
-                "max_depth":{'int':[3,50]}
+                "criterion": {'str': ['mse', 'friedman_mse', 'mae', 'poisson']},
+                "splitter":{'str': ['random', 'best']},
+                "max_features":{"str": ["auto", "sqrt", "log2"]},
+                "max_depth":{'int': [3, 50]}
             }
         ],
-        "randomforest":[
+        "randomforest": [
             ensemble.RandomForestRegressor,
             {
-                "criterion":{'str':['mse','mae']},
-                "n_estimators":{'int':[100,1000]},
-                "max_features":{"str":["auto", "sqrt", "log2"]},
-                "max_depth":{'int':[3,50]}
+                "criterion": {'str': ['mse', 'mae']},
+                "n_estimators":{'int': [100, 1000]},
+                "max_features":{"str": ["auto", "sqrt", "log2"]},
+                "max_depth":{'int': [3, 50]}
             }
         ],
-        "extratrees":[
+        "extratrees": [
             ensemble.ExtraTreesRegressor,
             {
-                "criterion":{'str':['mse','mae']},
-                "n_estimators":{'int':[100,1000]},
-                "max_features":{"str":["auto", "sqrt", "log2"]},
-                "max_depth":{'int':[3,50]}
+                "criterion": {'str': ['mse', 'mae']},
+                "n_estimators":{'int': [100, 1000]},
+                "max_features":{"str": ["auto", "sqrt", "log2"]},
+                "max_depth":{'int': [3, 50]}
             }
         ],
-        "gradientboosting":[
+        "gradientboosting": [
             ensemble.GradientBoostingRegressor,
             {
-                "criterion":{'str':['mse','friedman_mse']},
-                "n_estimators":{'int':[100,1000]},
-                "max_features":{"str":["auto", "sqrt", "log2"]},
-                "max_depth":{'int':[3,50]},
-                "loss":{'str':['ls','lad','huber']},
-                "learning_rate":{'float':[1e-3,0.1]}
+                "criterion": {'str': ['mse', 'friedman_mse']},
+                "n_estimators":{'int': [100, 1000]},
+                "max_features":{"str": ["auto", "sqrt", "log2"]},
+                "max_depth":{'int': [3, 50]},
+                "loss":{'str': ['ls', 'lad', 'huber']},
+                "learning_rate":{'float': [1e-3, 0.1]}
             }
         ],
-        "linearreg":[
+        "linearreg": [
             linear_model.LinearRegression,
             {}
         ],
-        "ridge":[
+        "ridge": [
             linear_model.Ridge,
             {
-                "alpha":{'float':[1e-3,0.1]},
-                "solver":{'str':['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga']},
-                "max_iter":{'int':[1000,5000]},
-                'tol':{'float':[1e-3,0.1]},
+                "alpha": {'float': [1e-3, 0.1]},
+                "solver":{'str': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga']},
+                "max_iter":{'int': [1000, 5000]},
+                'tol':{'float': [1e-3, 0.1]},
             }
         ],
-        "knn":[
+        "knn": [
             neighbors.KNeighborsRegressor,
             {
-                "n_neighbors":{'int':[3,10]},
-                "weights":{'str':['uniform','distance']},
-                "algorithm":{'str':['auto', 'ball_tree', 'kd_tree', 'brute']},
-                "p":{'int':[1,2]},
-                "leaf_size":{'int':[10,50]}
+                "n_neighbors": {'int': [3, 10]},
+                "weights":{'str': ['uniform', 'distance']},
+                "algorithm":{'str': ['auto', 'ball_tree', 'kd_tree', 'brute']},
+                "p":{'int': [1, 2]},
+                "leaf_size":{'int': [10, 50]}
             }
         ],
-        "poissonregressor":[
+        "poissonregressor": [
             linear_model.PoissonRegressor,
             {
-                "alpha":{'float':[1e-3,0.1]},
-                "fit_intercept":{'bool':[True,False]},
-                "max_iter":{'int':[1000,5000]},
-                'tol':{'float':[1e-3,0.1]},
+                "alpha": {'float': [1e-3, 0.1]},
+                "fit_intercept":{'bool': [True, False]},
+                "max_iter":{'int': [1000, 5000]},
+                'tol':{'float': [1e-3, 0.1]},
             }
         ],
-        "sgdregressor":[
+        "sgdregressor": [
             linear_model.SGDRegressor,
             {
-                "loss":{"str":['huber','epsilon_insensitive','squared_epsilon_insensitive','squared_loss']},
-                "penalty":{'str':['l2','l1','elasticnet']},
-                "alpha":{'float':[1e-4,1.0]},
-                "fit_intercept":{'bool':[True,False]},
-                "max_iter":{'int':[500,2000]},
-                "tol":{"float":[1e-3,1]},
-                "shuffle":{'bool':[True,False]},
-                "epsilon":{"float":[1e-1,1.5]},
-                "learning_rate":{"str":['invscaling','constant','optimal','adaptive']},
-                "early_stopping":{'bool':[True,False]}
+                "loss": {"str": ['huber', 'epsilon_insensitive', 'squared_epsilon_insensitive', 'squared_loss']},
+                "penalty":{'str': ['l2', 'l1', 'elasticnet']},
+                "alpha":{'float': [1e-4, 1.0]},
+                "fit_intercept":{'bool': [True, False]},
+                "max_iter":{'int': [500, 2000]},
+                "tol":{"float": [1e-3, 1]},
+                "shuffle":{'bool': [True, False]},
+                "epsilon":{"float": [1e-1, 1.5]},
+                "learning_rate":{"str": ['invscaling', 'constant', 'optimal', 'adaptive']},
+                "early_stopping":{'bool': [True, False]}
+            }
+        ], "radiusNeighboursRegressor": [
+            neighbors.RadiusNeighborsRegressor, {
+                "radius": {'float': [0.1, 1.0]},
+                "weights":{'uniform', 'distance'},
+                "algorithm": {'auto', 'ball_tree', 'kd_tree', 'brute'},
+                "p": {"int": [2, 10]},
+                "leaf_size":{"int": [30, 50]}
             }
         ]
     }
