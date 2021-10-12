f0a44482511add219739c52e2d937f3f4ba2da39
Devolta05 <sandyom07@gmail.com>
2021-10-12 18:07:24 +0530

    Added more parameters to the loss function for catboost regressor

--- blobcity/config/regressor_config.py
+++ blobcity/config/regressor_config.py
@@ -214,15 +214,16 @@ class regressor_config:
                 "booster":{'str':['gbtree', 'gblinear', 'dart']}
             }
         ]
-        "catboost":[
+        "CatBoostRegressor":[
             CatBoostRegressor,
             {
                 "learning_rate": {'float':[1e-3,0.1]},
                 "l2_leaf_reg":{'float':[1e-3, 5.0]},
                 "bootstrap_type":{'str':['bayesian', 'bernoulli', 'mvs', 'poisson', 'no']},
-                "loss_function":{'str':['RMSE']},
+                "loss_function":{'str':['RMSE', 'CrossEntropy', 'Logloss', 'MultiRMSE', 'MultiClass', 'Poisson', 'YetiRank', 'StochasticFilter']},
                 "max_iter":{'int':[1000, 10000]},
                 "max_depth":{'int':[3,50]},
+                "random_seed":{'int':[0,5,10]},
             }
         ],
     }
