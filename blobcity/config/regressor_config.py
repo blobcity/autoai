d1a5cdc1ed6cf4091252c23402a54a4907955527
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2021-11-01 21:04:55 +0800

    Fix OrthogonalMatchingPursuit is not in the correct config class

--- blobcity/config/regressor_config.py
+++ blobcity/config/regressor_config.py
@@ -291,5 +291,12 @@ class regressor_config:
                 "max_depth":{'int':[3,30]},
                 "tol":{'float':[1e-3,0.1]}
             }
+        ],
+        "OrthogonalMatchingPursuit":[
+            linear_model.OrthogonalMatchingPursuit,
+            {
+                "tol":{"float":[1e-3,0.1]},
+                "fit_intercept":{'bool':[True,False]}
+            }
         ]
     }
