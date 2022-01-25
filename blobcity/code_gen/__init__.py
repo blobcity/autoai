df934151720691e10b7e180808452d334992b77b
TRD <tilakgo@gmail.com>
2022-01-25 18:05:22 +0530

    Minor Code Changes in Code Generator

--- blobcity/code_gen/__init__.py
+++ blobcity/code_gen/__init__.py
@@ -1,4 +1,4 @@
 from .SourceCodes import SourceCode
 from .PyMeta import PyComments
 from .IpynbMeta import IpynbComments
-from .Generator import pycoder,codegen_type,yml_reader,code_generator,write_pycode,write_ipynbcode,initialize
+from .Generator import pycoder,codegen_type,yml_reader,code_generator,write_code,write_code,initialize
